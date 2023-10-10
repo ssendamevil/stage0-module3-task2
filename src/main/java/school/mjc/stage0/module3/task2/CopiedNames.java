f5c059cfa535fd4164c130c318348b0eba391956
Ali Orynbasar <96694797+ssendamevil@users.noreply.github.com>
2023-10-10 19:51:59 +0600

    solved 2

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -3,11 +3,11 @@ package school.mjc.stage0.module3.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable2 = 2000;
+        int variable_first = 2000;
         int value = 1000;
-        int variable3 = 4000;
-        int value2 = 3000;
+        int variable_second = 4000;
+        int value_first = 3000;
         int variables = 5000;
-        int value1 = 90000;
+        int value_second = 90000;
     }
 }
